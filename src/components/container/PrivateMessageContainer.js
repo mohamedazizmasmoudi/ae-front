cccbc54293dd1d2d3562564f6e9feb013f98f741
mohamedazizmasmoudi <55623593+mohamedazizmasmoudi@users.noreply.github.com>
2020-03-26 23:06:44 +0100

    opa

--- src/components/container/PrivateMessageContainer.js
+++ src/components/container/PrivateMessageContainer.js
@@ -4,9 +4,9 @@ import PrivateMessaging from '../PrivateMessaging';
 import axios from 'axios';
 import io from 'socket.io-client';
 
-const SOCKET_URL = "localhoost:8080";
+const SOCKET_URL = "http://localhost:8080";
 const socket = io(SOCKET_URL);
-const API_URL = 'localhoost:8080/api';
+const API_URL = 'http://localhost:8080/api';
 
 export default class PrivateMessagingContainer extends Component {
   constructor(props) {
